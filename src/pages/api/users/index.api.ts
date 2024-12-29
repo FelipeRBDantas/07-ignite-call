58c3140a84a1e8626cc7a6c7bdf23df4cf8c0147
feliperbdantas <feliperbdantas@outlook.com>
2024-12-29 15:36:58 -0300

    feat: Calendar Connection - Creating the User in the Adapter

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { prisma } from '@/lib/prisma'
 import { setCookie } from 'nookies'
+
+import { prisma } from '@/lib/prisma'
 
 export default async function handler(
   req: NextApiRequest,
