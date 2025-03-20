8f067961042bddc562f0ff0ec26733c59014bf1e
feliperbdantas <feliperbdantas@outlook.com>
2025-03-20 20:36:32 -0300

    feat: Calendar and Availability - Creating an Event on Google

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -2,7 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import dayjs from 'dayjs'
 
+import { google } from 'googleapis'
+
 import { prisma } from '@/infra/prisma/prisma'
+
+import { getGoogleOAuthToken } from '@/infra/google/google'
 
 import { createSchedulingBodySchema } from './schedule.schema'
 
@@ -49,13 +53,47 @@ export default async function handler(
       .json({ message: 'There is another scheduling at the same time.' })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       observations,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [
+        {
+          email,
+          displayName: name,
+        },
+      ],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
