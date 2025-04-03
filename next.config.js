c45afa07581978ac1f9f05ea4d0decfb2b952089
feliperbdantas <feliperbdantas@outlook.com>
2025-04-03 15:22:23 -0300

    fix: Project Deployment - Adjustments

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
-  output: 'standalone',
-  experimental: {
-    serverActions: true,
-  },
 }
 
 module.exports = nextConfig
